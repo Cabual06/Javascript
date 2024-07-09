838b1d21b5ab164cb457f6ba89462c791001b304
Cabual <efcabual09@gmail.com>
2024-07-09 18:16:40 +0800

    Javascript

---
+++ 57-EventListener/listerner.js
@@ -0,0 +1,88 @@
+
+
+// eventlistener = Listen for specific events to create interactive web pages.
+
+// events: CLICK, MOUSEOVER, MOUSEOUT
+// .addEventListener(event, callback); ex .addEventListener(event, changecolor);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+const myBox = document.getElementById('myBox');
+
+
+// myBox.addEventListener("click", changeColor);
+
+// function changeColor(event){
+//     event.target.style.backgroundColor = "blue";
+//     event.target.textContent = "You clicked";
+// }
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+//-- ANOTEHR WAY -- //
+
+// myBox.addEventListener("click", function(event){
+//     event.target.style.backgroundColor = "blue";
+//     event.target.textContent = "You clicked";
+// });
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+//-- ARROW FUNCTION --//   NO NEED TO ENCLODE () IF YOU HAVE ONLY 1 PARAMETER
+
+myBox.addEventListener("click", event => {
+    event.target.style.backgroundColor = "blue";
+    event.target.textContent = "You clicked";
+});
+
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+const box2 = document.getElementById("box2");
+
+box2.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "pink";
+    event.target.textContent = "Hovered";
+})
+
+box2.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "lightblue";
+    event.target.textContent = "Box 2:";
+})
+
+
+//////////////////////////////////////////////////////////////////////
+
+const myButton = document.getElementById("myButton");
+
+// myButton.addEventListener("mouseover", (event) => {
+//     event.target.style.backgroundColor = "red";
+//     event.target.textContent = "Hovered";
+// })
+
+// myButton.addEventListener("mouseout", (event) => {
+//     event.target.style.backgroundColor = "green";
+//     event.target.textContent = "Click Me";
+// })
+
+myButton.addEventListener("click", (event) => {
+    box2.style.backgroundColor = "orange";
+    box2.textContent = "Clicked";
+})
+myButton.addEventListener("mouseover", (event) => {     // CHANGE THE STATE OF BOX2 ON HOVER..REPLACE (event.target) to the VARIABLE OF SELECTED ITEM
+    box2.style.backgroundColor = "red";
+    box2.textContent = "Hovered";
+})
+
+myButton.addEventListener("mouseout", (event) => {
+    box2.style.backgroundColor = "green";
+    box2.textContent = "Click Me";
+})
