4cf4bc0f1fe2276bb0ac7c050cc26cabe0c4c278
Cabual <efcabual09@gmail.com>
2024-07-10 20:36:26 +0800

    Javascript

--- pra.js
+++ pra.js
@@ -1,11 +1,29 @@
+const box1 = document.getElementById('box1');
+
+box1.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = "orange";
+})
 
 
-const fruits = ["apple", "banan", "orange"];
+const box2 = document.getElementById('box2');
 
 
-fruits.forEach(display);
+box2.addEventListener("click", (event) => {
+    box2.style.backgroundColor = "red";
 
-function display(elements){
-    console.log(elements);
-}
+})
 
+
+const button1 = document.getElementById('button1');
+
+button1.addEventListener("click", (event) => {
+    box1.style.backgroundColor = "pink";
+    box1.textContent = "Button 1 Press";
+})
+
+const button2 = document.getElementById('button2');
+
+button2.addEventListener("click", (event) => {
+    box2.style.backgroundColor = "yellow";
+    box2.textContent = "Button 2 Press";
+})
